6dda34ddd0c3f60e8ac6ddd0f682bf204be29baa
ouramdaneMakhoukh <oumakhoukh@gmail.com>
2019-03-28 17:15:10 +0100

    seed cat ok

--- src/main/java/org/sid/web/ProduitController.java
+++ src/main/java/org/sid/web/ProduitController.java
@@ -15,11 +15,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cloudinary.Cloudinary;
 
@@ -56,6 +56,7 @@ public class ProduitController {
 		model.addAttribute("motCle", mc);
 		
 		return "prod";
+		
 	}
 
 	
